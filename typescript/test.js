b34d07862dbcdb989ae2fc1c9cb4f43f4fb9b02e
mrzsping <18090749615@163.com>
2019-03-14 14:34:32 +0800

    5

--- typescript/test.js
+++ typescript/test.js
@@ -1,14 +1,5 @@
-var Student = /** @class */ (function () {
-    function Student(first, middle, last) {
-        this.first = first;
-        this.middle = middle;
-        this.last = last;
-        this.fullName = first + '' + middle + '' + last;
-    }
-    return Student;
-}());
-function Per(person) {
-    return 'hello ' + person.first + person.last;
+function greeter(person) {
+    return "Hello, " + person.first;
 }
-var user = new Student("Jane", "M.", "User");
-document.body.innerHTML = Per(user);
+var user = { first: 'xiao', last: 'ming' };
+document.body.innerHTML = greeter(user);
