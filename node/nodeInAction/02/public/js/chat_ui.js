ce9000717540a36f3d4587f89d986c8220a0e424
mrzsping <18090749615@163.com>
2019-03-22 11:59:40 +0800

    23

---
+++ node/nodeInAction/02/public/js/chat_ui.js
@@ -0,0 +1,22 @@
+function divEscapedContentElement(message){
+  return $('<div></div>').text(message);
+}
+function divSystemContentElement(message){
+  return $('<div></div>').html('<i>' + message + '</i>');
+}
+function processUserInput(chatApp, socket){
+  let message = $('#send-message').val();
+  let systemMessage;
+
+  if(message.charAt(0) == '/'){
+    systemMessage = chatApp.processCommand(message);
+    if(systemMessage){
+      $('#messages').append(divSystemContentElement(systemMessage));
+    }
+  }else{
+    chatApp.sendMessage($('#room').text(), message);
+    $('#messages').append(divEscapedContentElement(message));
+    $('#messages').scrollTop($('#messages').prop('scrollHeight'));
+  }
+  $('#send-message').val('');
+}
