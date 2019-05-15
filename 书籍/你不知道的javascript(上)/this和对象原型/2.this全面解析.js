b8fa86db250a89296e1ea619cee5b815cdbba1a0
mrzsping <18090749615@163.com>
2019-05-15 18:44:27 +0800

    58

--- 书籍/你不知道的javascript(上)/this和对象原型/2.this全面解析.js
+++ 书籍/你不知道的javascript(上)/this和对象原型/2.this全面解析.js
@@ -32,4 +32,21 @@ var bar3 = function(){ // 无法改变this
 bar3();
 setTimeout(bar, 100)
 
+//    new绑定: 被new操作符调用的普通函数
+//     步骤: 1.创建一个全新对象 -> 2.执行原型连接 -> 3.绑定到函数调用的this -> 4.如没返回对象，则自动返回对象
 
+
+//    优先级: new > 显式 > 隐式 > 默认
+
+//    DMZ对象
+
+function foo(a,b) {
+    console.log( "a:" + a + ", b:" + b );
+}
+// 我们的DMZ 空对象
+var ø = Object.create( null );
+// 把数组展开成参数
+foo.apply( ø, [2, 3] ); // a:2, b:3
+// 使用bind(..) 进行柯里化
+var bar = foo.bind( ø, 2 );
+bar( 3 ); // a:2, b:3
