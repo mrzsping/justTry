24f989e6c47eb4f78b6ffc853bc62f62af820f09
mrzsping <18090749615@163.com>
2019-06-13 23:02:03 +0800

    2

--- java/pro04/src/cn/zsp/collection/TestMyCollection.java
+++ java/pro04/src/cn/zsp/collection/TestMyCollection.java
@@ -9,6 +9,13 @@ public class TestMyCollection<E>{
         elementData = new Object[DEFAULT_CAPACITY];
     }
     public TestMyCollection(int capacity){
+        if (capacity < 0){
+            throw new RuntimeException("容器的容量不能为负数");
+        } else if(capacity == 0){
+            elementData = new Object[DEFAULT_CAPACITY];
+        }else {
+            elementData = new Object[capacity];
+        }
         elementData = new Object[capacity];
     }
 //    @Override
@@ -21,7 +28,6 @@ public class TestMyCollection<E>{
 //        sb.setCharAt(sb.length() - 1, ']');
 //        return sb.toString();
 //    }
-
     public void add(E element){
         // 扩容
         if (size == elementData.length) {
@@ -31,12 +37,32 @@ public class TestMyCollection<E>{
         }
         elementData[size++] = element;
     }
+    public void remove(E element){
+        for(int i = 0; i < size; i++){
+            if(element.equals(get(i))){
+                remove(i);
+            }
+        }
+    }
+    public int size(){
+        return size;
+    }
+    public boolean isEmpty(){
+        return size == 0 ? true : false;
+    }
+    public void remove(int index){
+        int l = elementData.length - 1 - index;
+        if(l>0){
+            System.arraycopy(elementData, index + 1,elementData,index, l);
+        }
+        elementData[--size] = null;
+    }
     public static void main(String[] args) {
         TestMyCollection t = new TestMyCollection(20);
         for(int i = 0; i < 40; i++){
             t.add("sda"+i);
         }
-        System.out.println(t);
+        System.out.println(t.get(40));
     }
     public String toString(){
         StringBuilder b = new StringBuilder();
@@ -47,6 +73,20 @@ public class TestMyCollection<E>{
         b.setCharAt(elementData.length - 1, ']');
 
         return b.toString();
+    }
+    // set/get方法
+    public E get(int index){
+        checkRange(index);
+        return (E)elementData[index];
+    }
+    public void set(E element, int index){
+        checkRange(index);
+        elementData[index] = element;
+    }
+    public void checkRange(int index){
+        if (0 > index || index > size -1) {
+            throw new RuntimeException("索引不合法");
+        }
     }
 
 }
